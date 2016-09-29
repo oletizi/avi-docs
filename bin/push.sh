b3ce9b9b94d16e8bf6bc50007268e899499fa3b5
Orion Letizi <oletizi@mac.com>
2016-09-28 18:25:16 -0700

    - small fix to header - lorem ipsum on homepage - updated the push script

--- bin/push.sh
+++ bin/push.sh
@@ -3,20 +3,21 @@
 mydir=`dirname $0`
 cd $mydir
 mydir=`pwd`
+
+$mydir/render.sh
+
 cd $mydir/../src/site/_site
 
-prod1="ubuntu@unknown:/path/to/document/root/"
-prod2="ubuntu@unknown:/path/to/document/root/"
+prod1="ubuntu@docs:/var/www/html/"
 
 ssh="ssh"
 rsync="rsync"
 options="-avh --delete --cvs-exclude"
-destinations=""
+destinations="${prod1} ${prod2}"
 
 if [ -z $1 ]; then
     echo "Pushing to production"
-    options="${options} -e \"${ssh}\""
-    destinations="${prod1} ${prod2}"
+    options="-e ${ssh} ${options}"
 elif [ "dev" = $1 ]; then
     echo "Pushing to local development site: $1";
     destinations="${HOME}/.avi-docs-site/"
@@ -35,5 +36,5 @@ for dest in $destinations; do
     cmd="$rsync $options . $dest"
     echo "Executing from: " . `pwd`
     echo "CMD: ${cmd}"
-    $cmd
+    exec ${cmd}
 done
--- src/site/index.md
+++ src/site/index.md
@@ -1,4 +1,14 @@
 ---
-title: Latest Posts
+title: Avi Documentation
 layout: default
 ---
+
+Welcome to the Avi Networks online documentation.
+
+We should come up with something really cool to put on the homepage.
+
+Try clicking on the navigation links on the left to see what's in here!
+
+<===== CLICK HERE!
+
+<div style="margin-top:500px">&nbsp;</div>
