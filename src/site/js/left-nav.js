c4d64badca6b03bdb34433a0bf5435610163c116
Orion Letizi <oletizi@mac.com>
2017-01-15 11:51:18 -0800

    style updates to left-nav

---
+++ src/site/js/left-nav.js
@@ -0,0 +1,67 @@
+
+$(document).ready(function () {
+    var cookieName = 'expandable-nav-v1.1';
+
+    function setActive() {
+        var url = window.location;
+        var parser = document.createElement('a');
+        parser.href = url;
+        var fullPath = parser.pathname;
+        var shortPath = fullPath.replace(/\/$/, "");
+
+        $('ul#expandable-nav a[href="' + fullPath + '"]').parent().addClass('active');
+        $('ul#expandable-nav a[href="' + shortPath + '"]').parent().addClass('active');
+    }
+
+    function getExpanded() {
+        var tmp = Cookies.getJSON(cookieName);
+        var expanded;
+        if (!Array.isArray(tmp)) {
+            expanded = [];
+        } else {
+            expanded = tmp;
+        }
+        return expanded;
+    }
+
+    function containsObject(list, obj) {
+        var i;
+        for (i = 0; i < list.length; i++) {
+            if (list[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    setActive();
+
+    var expanded = getExpanded();
+
+    if (Array.isArray(expanded)) {
+        $("#expandable-nav .collapse").removeClass('in');
+        for (var i = 0; i < expanded.length; i++) {
+            $(expanded[i]).collapse("show");
+        }
+    }
+
+    $("[data-toggle='collapse']").on("click", function () {
+        var dataTarget = this.attributes['data-target'].value;
+        var tmp = Cookies.getJSON(cookieName);
+        var expanded;
+        if (!Array.isArray(tmp)) {
+            expanded = [];
+        } else {
+            expanded = tmp;
+        }
+        var index = expanded.indexOf(dataTarget);
+        if (index >= 0) {
+            // remove it
+            expanded.splice(index, 1);
+        } else {
+            // add it
+            expanded.push(dataTarget);
+        }
+        Cookies.set(cookieName, expanded);
+    });
+})
