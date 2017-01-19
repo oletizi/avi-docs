be159dcd3c0530bbc88fc5e84b928310b23ca8ea
Orion Letizi <oletizi@mac.com>
2017-01-19 11:02:39 -0800

    modified push to accept arbitrary push destinations

--- bin/push.sh
+++ bin/push.sh
@@ -4,37 +4,18 @@ mydir=`dirname $0`
 cd $mydir
 mydir=`pwd`
 
-$mydir/render.sh
-
-cd $mydir/../src/site/_site
-
-prod1="ubuntu@docs:/var/www/html/"
-
 ssh="ssh"
 rsync="rsync"
 options="-avh --delete --cvs-exclude"
-destinations="${prod1} ${prod2}"
-
 if [ -z $1 ]; then
-    echo "Pushing to production"
-    options="-e ${ssh} ${options}"
-elif [ "local" = $1 ]; then
-    destinations="${HOME}/.avi-docs-site/"
-    echo "Pushing to local site: ${destination}";
-    if [ ! -e $destinations ]; then
-        echo "Creating local dev site directory: ${destinations}"
-        mkdir $destinations
-    fi
+    echo "No destinations specified. Doing nothing."
 else
-    echo "Usage:"
-    echo ""
-    echo "  push.sh [dev]"
-    exit
+    $mydir/render.sh
+    cd $mydir/../src/site/_site
+    for dest in "$@"; do
+        cmd="$rsync $options . $dest"
+        echo "Executing from: " . `pwd`
+        echo "CMD: ${cmd}"
+        exec ${cmd}
+    done
 fi
-
-for dest in $destinations; do
-    cmd="$rsync $options . $dest"
-    echo "Executing from: " . `pwd`
-    echo "CMD: ${cmd}"
-    exec ${cmd}
-done
